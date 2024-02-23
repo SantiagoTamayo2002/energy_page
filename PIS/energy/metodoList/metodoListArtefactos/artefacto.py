07782710186521a33ff920ef5321684c8d0cec09
Christian Leonardo Robles Maza <christian.l.robles@unl.edu.ec>
2024-02-22 20:04:28 -0500

    Documentación del codigo
    Agregue comentarios para entender mejor

--- PIS/energy/metodoList/metodoListArtefactos/artefacto.py
+++ PIS/energy/metodoList/metodoListArtefactos/artefacto.py
@@ -1,24 +1,32 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
-
+# Importar el modelo Artefacto desde energy.models
 from energy.models import Artefacto
 
+# Función para guardar un nuevo artefacto en la base de datos
 def guardar_artefacto(request, form):
+    # Guardar el artefacto del formulario en la variable artefacto
     artefacto = form.save(commit=False)
-    # artefacto ya existe dentro del usuario
+    # Verificar si el artefacto ya existe para el usuario actual
     if Artefacto.objects.filter(user=request.user, nombre_artefacto=artefacto.nombre_artefacto).exists():
+        # Mostrar un mensaje de error si el artefacto ya existe        
         return messages.error(request, 'El artefacto ya existe')
     else:
+        # Asignar el usuario actual al artefacto y guardarlo en la base de datos
         artefacto.user = request.user
         artefacto.save()
+        # Mostrar un mensaje de éxito indicando que el artefacto se agregó correctamente        
         return messages.success(request, 'El artefacto se agrego correctamente')
 
 
 
 
 
-
+# Función para eliminar un artefacto de la base de datos
 def eliminar_artefacto(request, artefacto_id):
+    # Obtener el objeto Artefacto correspondiente al artefacto_id especificado
     artefacto = Artefacto.objects.get(pk=artefacto_id)
+    # Eliminar el artefacto de la base de datos    
     artefacto.delete()
+    # Redirigir al usuario a la página de artefactos después de eliminar exitosamente el artefacto
     return redirect('artefacto')
--- PIS/energy/metodoList/metodoListInforme/informe.py
+++ PIS/energy/metodoList/metodoListInforme/informe.py
@@ -2,11 +2,14 @@ from django.db.models import Sum
 from energy.models import Informe
 from datetime import datetime
 
-
+# Función para calcular el consumo total mensual de un usuario
 def calcular_consumo_total_mensual(usuario):
+    # Obtener la suma del consumo total de todos los informes del usuario en el mes actual   
     consumo_mensual = Informe.objects.filter(user=usuario).aggregate(Sum('consumo_total'))[
                           'consumo_total__sum'] or 0
+    # Convertir el consumo de Wh a kWh si el consumo mensual es diferente de cero    
     if consumo_mensual != 0:
         consumo_mensual = consumo_mensual / 1000
+    # Retornar el consumo total mensual    
     return consumo_mensual
 
